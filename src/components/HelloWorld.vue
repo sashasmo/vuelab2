4cd58268e35e6310c8a2dce28ffe29a97888915b
Смотрицька Олександра Романівна <smotritskaya.sasha@example.com>
2024-10-08 23:44:53 +0300

    Vue.js lab 1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    <strong>Спеціальність:</strong> 121 ІПЗ<br />
+    <strong>Курс:</strong> 2 курс магістр<br />
+    <strong>Група:</strong> 8.1213<br />
+    <strong>ПІБ:</strong> Смотрицька Олександра Романівна
+  </p>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
